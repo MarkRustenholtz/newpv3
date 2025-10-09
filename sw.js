0548703342b0bde8c6efa0ae0fd0371ce2416f00
MarkRustenholtz <markrusten@gmail.com>
2025-10-09 23:16:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "app-pv-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.webmanifest",
+  "./logo-rf.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Installation
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activation (nettoyage des anciens caches)
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+// Fetch (réponses hors ligne)
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res =>
+      res ||
+      fetch(e.request).then(r => {
+        const clone = r.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return r;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
