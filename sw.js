e4e9eaa8610db9782d0e0feca97e0c35e905cc33
MarkRustenholtz <markrusten@gmail.com>
2025-10-10 00:35:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app-pv-v5";
+const CACHE_NAME = "app-pv-v1";
 const ASSETS = [
   "./",
   "./index.html",
@@ -39,6 +39,7 @@ self.addEventListener("fetch", e => {
     )
   );
 });
+
 
 
 
